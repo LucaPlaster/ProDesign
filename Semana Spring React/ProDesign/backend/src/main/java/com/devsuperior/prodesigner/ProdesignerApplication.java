0f5e77a14c2b76f4735fe7e3ccce446aaca10c40
Luca Plaster <lucaplaster@gmail.com>
2022-07-11 14:09:51 -0300

    Primeira versao

---
+++ Semana Spring React/ProDesign/backend/src/main/java/com/devsuperior/prodesigner/ProdesignerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.prodesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdesignerApplication.class, args);
+	}
+
+}
---
+++ Semana Spring React/ProDesign/backend/src/test/java/com/devsuperior/prodesigner/ProdesignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.prodesigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdesignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
