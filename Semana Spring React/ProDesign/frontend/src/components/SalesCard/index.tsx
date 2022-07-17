b0d6a5e0b890c32cae562661a8708671aa75e50b
Luca Plaster <lucaplaster@gmail.com>
2022-07-17 11:21:48 -0300

    Sale listing

--- Semana Spring React/ProDesign/frontend/src/components/SalesCard/index.tsx
+++ Semana Spring React/ProDesign/frontend/src/components/SalesCard/index.tsx
@@ -3,21 +3,24 @@ import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from "../NotificationButton";
+import { BASE_URL } from "../../utils/request";
 import "./styles.css";
+import { Sale } from "../../models/sale";
 
 function SalesCard() {
+  const max = new Date();
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
 
-    const max = new Date();
-    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+  const [minDate, setMinDate] = useState(min);
+  const [maxDate, setMaxDate] = useState(max);
 
-    const [minDate, setMinDate] = useState(min);
-    const [maxDate, setMaxDate] = useState(max);
+  const [sales, setSales] = useState<Sale[]>([]);
 
-    useEffect(() => {
-      axios.get("http://localhost:8080/sales").then(response => {
-        console.log(response.data);
-      })
-    }, []);
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales`).then((response) => {
+      setSales(response.data.content);
+    });
+  }, []);
 
   return (
     <div className="ProDesign-card">
@@ -54,45 +57,23 @@ function SalesCard() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th className="show992">#341</th>
-              <th className="show576">11/07/2022</th>
-              <th>Anakin</th>
-              <th className="show992">15</th>
-              <th className="show992">11</th>
-              <th>R$ 55300.00</th>
-              <th>
-                <div className="ProDesign-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </th>
-            </tr>
-            <tr>
-              <th className="show992">#341</th>
-              <th className="show576">11/07/2022</th>
-              <th>Anakin</th>
-              <th className="show992">15</th>
-              <th className="show992">11</th>
-              <th>R$ 55300.00</th>
-              <th>
-                <div className="ProDesign-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </th>
-            </tr>
-            <tr>
-              <th className="show992">#341</th>
-              <th className="show576">11/07/2022</th>
-              <th>Anakin</th>
-              <th className="show992">15</th>
-              <th className="show992">11</th>
-              <th>R$ 55300.00</th>
-              <th>
-                <div className="ProDesign-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </th>
-            </tr>
+            {sales.map((sale) => {
+              return (
+                <tr key={sale.id}>
+                  <th className="show992">{sale.id}</th>
+                  <th className="show576">{new Date(sale.date).toLocaleDateString()}</th>
+                  <th>{sale.seller_name}</th>
+                  <th className="show992">{sale.visited}</th>
+                  <th className="show992">{sale.deals}</th>
+                  <th>R$ {sale.amount.toFixed(2)}</th>
+                  <th>
+                    <div className="ProDesign-red-btn-container">
+                      <NotificationButton />
+                    </div>
+                  </th>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
